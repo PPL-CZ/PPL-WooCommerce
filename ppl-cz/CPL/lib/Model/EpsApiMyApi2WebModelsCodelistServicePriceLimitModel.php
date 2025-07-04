2db23b4750739f22d626e8e34ec33aeedfc35506
rex2630 <rex2630@gmail.com>
2025-07-04 11:31:45 +0200

    Explicitely mark nullable type parameters as such

--- ppl-cz/CPL/lib/Model/EpsApiMyApi2WebModelsCodelistServicePriceLimitModel.php
+++ ppl-cz/CPL/lib/Model/EpsApiMyApi2WebModelsCodelistServicePriceLimitModel.php
@@ -206,7 +206,7 @@ class EpsApiMyApi2WebModelsCodelistServicePriceLimitModel implements ModelInterf
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->container['service'] = $data['service'] ?? null;
         $this->container['currency'] = $data['currency'] ?? null;
